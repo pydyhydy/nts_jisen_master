e9907fbd7cd9370af805befbcaa4285ced784f13
pydyhydy <pydyhydy@gmail.com>
2018-09-11 14:52:44 +0900

    try to fix base.html problem, take 1

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ Hope it works well.
 
 chmod +x manage.py, at WSL with DrvFS
 LF and chmod again
+added: 
+    TEMPLATE_DIRS = [TEMPLATES[0]['DIRS'][0]]
--- nts_jisen_master/settings.py
+++ nts_jisen_master/settings.py
@@ -225,6 +225,7 @@ TEMPLATES = [
         },
     },
 ]
+TEMPLATE_DIRS = [TEMPLATES[0]['DIRS'][0]]
 
 if DJANGO_VERSION < (1, 9):
     del TEMPLATES[0]["OPTIONS"]["builtins"]
